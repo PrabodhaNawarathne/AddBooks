d2de1271334849471b05fadee0e190b1e07c580d
prna1 <prabodha.nawarathne@ebuilder.com>
2019-05-18 00:13:03 +0530

    Initial laravel project and insert form data to database functionality

---
+++ resources/views/books/create.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Laravel 5.5 CRUD Tutorial With Example From Scratch </title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  </head>
+  <body>
+    <div class="container">
+      <h2>Create A Book</h2><br  />
+      <form method="post" action="{{url('books')}}">
+      {{csrf_field()}}
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <label for="name">Title</label>
+            <input type="text" class="form-control" name="title">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="price">Author</label>
+              <input type="text" class="form-control" name="author">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <button type="submit" class="btn btn-primary float-right" style="margin-left:38px">Add Book</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
